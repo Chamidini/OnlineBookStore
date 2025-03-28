2a176279ec7d517f00813b3569a9f618f0dee1f1
Chamidini <swethachamidini@gmail.com>
2025-03-29 01:05:24 +0530

    add routes, route

--- project/src/App.js
+++ project/src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Category from './pages/Category'; 
+import Cart from './pages/Cart'; 
+import Checkout from './pages/Checkout'; 
+
+
 function App() {
   return (
     <div className="App">
       <Header/>
 
-
+      <Routes>
+        <Route path='/home' element={<Home/>} />
+        <Route path='/category' element={<Category/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/checkout' element={<Checkout/>} />
+      </Routes>
+      
       <Footer/>
     </div>
   );
