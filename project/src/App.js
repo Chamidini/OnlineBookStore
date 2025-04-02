030b1fb1314a08e32ed95286ec041abd99cd3e7c
Chamidini <swethachamidini@gmail.com>
2025-04-03 01:42:06 +0530

    check ruotes

--- project/src/App.js
+++ project/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './components/Login';
 
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Category from './pages/Category'; 
 import Cart from './pages/Cart'; 
 import Checkout from './pages/Checkout'; 
+import Createanaccount from './components/Create-an-account';
 
 
 function App() {
@@ -15,12 +17,16 @@ function App() {
     <div className="App">
       <Header/>
 
-      <Routes>
+      <Createanaccount/>
+
+      {/* <Login/> */}
+
+      {/* <Routes>
         <Route path='/home' element={<Home/>} />
         <Route path='/category' element={<Category/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/checkout' element={<Checkout/>} />
-      </Routes>
+      </Routes> */}
       
       <Footer/>
     </div>
