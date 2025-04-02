80089801f3f9e9dd42d6187b16dd47c9f1922262
Chamidini <swethachamidini@gmail.com>
2025-04-02 19:46:08 +0530

    create login form

--- project/src/components/Login.js
+++ project/src/components/Login.js
@@ -1,1 +1,23 @@
+import React from 'react'
+import "./Login.css"
 
+export default function Login() {
+  return (
+    <div className='page'>
+      <div className='cover'>
+        <h1>Book Shop</h1>
+        <input type='text' className='input' placeholder='Username'/>
+        <input type='password' className='input' placeholder='Password'/>
+
+        <div className='login-btn'>Log in</div>
+
+        <p className='text'><a href='#'>Forgot Password?</a></p>
+
+        <p className='text'>or</p>
+
+        <div className='signup-btn'>Sign up</div>
+
+      </div>
+    </div>
+  )
+}
