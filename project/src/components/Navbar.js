07d4b16d8dd0cae7f6e46b214032c201b78ec7a4
Chamidini <swethachamidini@gmail.com>
2025-04-09 02:12:42 +0530

    create new navbar

--- project/src/components/Navbar.js
+++ project/src/components/Navbar.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 export default function Narbar() {
+// export const Navbar = () => {
+  
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <nav>
       
       <img src="./pic/logo.jpg" alt="logo" className='logo' height={100} width={120}></img>
 
-      <ul>
-        <li><Link to='/home'>Home</Link></li>
-        <li><Link to='category'>Category</Link></li>
-        <li><Link to='cart'>Cart</Link></li>
-        <li><Link to='/checkout'>Checkout</Link></li>
+      <div className='menu' onClick={() =>{
+        setMenuOpen(!menuOpen);
+      }}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className='{menuOpen ? "open" : ""}'>
+        <li><NavLink  to='/home'>Home</NavLink ></li>
+        <li><NavLink  to='category'>Category</NavLink ></li>
+        <li><NavLink to='cart'>Cart</NavLink></li>
+        <li><NavLink  to='/checkout'>Checkout</NavLink ></li>
       </ul>
     </nav>
   )
