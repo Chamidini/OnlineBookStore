e8ab0352b8fcd262591136bc61d6fbef994d0ae0
Chamidini <swethachamidini@gmail.com>
2025-09-15 00:28:52 +0530

    update the Create an account page(input fields)

--- project/src/components/Create-an-account.js
+++ project/src/components/Create-an-account.js
@@ -7,18 +7,41 @@ export default function Createanaccount() {
       <div className='cover'>
         <h1>Create an Account</h1>
 
-        <p>First Name <input type='text' className='input'/></p>
+        {/* <div className='input feilds'>
+          <p>First Name <input type='text' className='input'/></p>
 
-        <p>Last Name <input type='text' className='input'/></p>
+          <p>Last Name <input type='text' className='input'/></p>
 
-        <p>Email<input type='text' className='input'/></p>
+          <p>Email<input type='text' className='input'/></p>
+
+          <p>Password <input type='password' className='input'/></p>
+        </div> */}
+
+        <div className="form-group">
+          <label>First Name</label>
+          <input type="text" className="input" />
+        </div>
+
+        <div className="form-group">
+          <label>Last Name</label>
+          <input type="text" className="input" />
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input type="text" className="input" />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" className="input" />
+        </div>
+          
+        <div className='Createaccount'>
+          <button className='create-account-btn'>Create Account</button>
+          <p className='text'>Already have an account? <a href='#'>Log In</a></p>
+        </div>
         
-        <p>Password <input type='password' className='input'/></p>
-
-        <button className='create-account-btn'>Create Account</button>
-
-        <p className='text'>Already have an account? <a href='#'>Log In</a></p>
-
       </div>
     </div>
   )
