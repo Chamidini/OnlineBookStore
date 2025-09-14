36510e4800370163f8113b19ec6d4a9bbe995ba0
Chamidini <swethachamidini@gmail.com>
2025-09-14 23:37:46 +0530

    fix the error  for '        <Route path='/signup' element={<Createanaccount/>} /> '

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './components/Login';
 
@@ -9,24 +10,33 @@ import Home from './pages/Home'
 import Category from './pages/Category'; 
 import Cart from './pages/Cart'; 
 import Checkout from './pages/Checkout'; 
+import BookDetails from './pages/BookDetails';
 import Createanaccount from './components/Create-an-account';
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* <Header/> */}
 
-      <Createanaccount/>
+      <Navbar/>
+
+      {/* <Createanaccount/> */}
 
       {/* <Login/> */}
 
-      {/* <Routes>
+      {/* <BookDetails/> */}
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
         <Route path='/home' element={<Home/>} />
         <Route path='/category' element={<Category/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/checkout' element={<Checkout/>} />
-      </Routes> */}
+        <Route path='/book-details' element={<BookDetails />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Createanaccount/>} />
+      </Routes>
       
       <Footer/>
     </div>
